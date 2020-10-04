f0de578c26d6b2a5f49ed87eb96eb0f446a5f699
Matt-Copenhaver <matt.copenhaver@gmail.com>
2020-10-04 15:47:18 -0600

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "visitorus-test-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://495hfbebad.execute-api.us-east-2.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_Cm7ooRzvM",
+    APP_CLIENT_ID: "kh9f1ep92vrfvkek9993l5dp4",
+    IDENTITY_POOL_ID: "us-east-2:9a987c0a-45a3-4640-87de-fa1ae025196d"
+  }
+};
